845200d7043e711aa990c0eb126ceb90ecf945f6
TroySandy <troysandy16@gmail.com>
2021-05-25 15:53:26 -0400

    final touches needed

--- src/config.tsx
+++ src/config.tsx
@@ -4,6 +4,8 @@ const config = {
       ? "http://localhost:4000"
       : "https://frozen-eyrie-44849.herokuapp.com",
   REACT_APP_UNSPLASH_APIKEY: "QrZ_4CjnXpZU7ZOT1EJBrOjpWbgZyRvfZQ6QfutowmE",
+  REACT_APP_UNSPLASH_APIKEY_SECRET:
+    "0cgoyxYPr_bs0UqvMhCwUGt1Xfdss6wXAjVQz_4-frw",
 };
 
 export default config;
